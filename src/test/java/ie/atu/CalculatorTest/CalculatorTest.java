79fe6b010eab36c0ec78d6362d6a041b02947503
G00399850@atu.ie <g00399850@atu.ie>
2023-03-30 11:53:22 +0100

    Created new classes

---
+++ src/test/java/ie/atu/CalculatorTest/CalculatorTest.java
@@ -0,0 +1,14 @@
+package ie.atu.CalculatorTest;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest{
+    Calculator myCalc;
+
+    @Test
+    public void testAdd()
+    {
+        myCalc = new Calculator();
+        assertEquals(40,myCalc.add(20,20);
+    }
+}
